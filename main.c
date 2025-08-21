c3592d21f13cfedf18ead9e67e46cf54c5d957f4
W9D10 <damian.otazu91@gmail.com>
2025-08-21 14:23:13 -0300

    Se corrigieron los problemas en la represantacion de la imagen y se acomodaron algunas cosas

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-//#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include ".\\include\\raylib.h"
@@ -34,10 +33,10 @@ int CharToInt(char n[],int len){
 /*Recibe un array de caracteres el cual es usado para representar * 
  * los colores en Formato RGB, pinta pixel por pixel la imagen    *
  * en la ventana                                                  */
-void DrawImg2(unsigned char buffer[],Color c,int w,int h,int size){
+void DrawImg2(unsigned char buffer[],Color c,int w,int h,int ini,int size){
   int x = 0;
   int y = 0;
-  for(int i = 0; i < size-3;i+=3){
+  for(int i = ini+1; i < size-3;i+=3){
     c.a = 255;
     c.r = buffer[i+0];
     c.b = buffer[i+2];
@@ -85,11 +84,24 @@ int LenHeader(unsigned char header[]){
   return len-1;
 }
 
+/*Busca el caracter '\0', el cual es usado para*
+ * medir la longitud re una cadena.            */
+int LenChar(char c[]){
+  int len = 0;
+  while(c[len] != '\0'){
+    len ++;
+  }
+  return len;
+}
+
+
 /* Separea  la cabecera, corrobora que el archivo sea PPM y devuelve*
  * el ancho y alto de la imagen.                                    */
-void CutHeader(unsigned char header[],char width[],char height[],int len){
+void CutHeader(unsigned char header[],int *width,int *height,int len){
   char nm[4];
   char mc[4];
+  char auxw[256];
+  char auxh[256];
   int saltos = 0;
   char esp;
   int cont = 0;
@@ -107,10 +119,12 @@ void CutHeader(unsigned char header[],char width[],char height[],int len){
       nm[i] = header[i];
     }
     if(saltos > 0 && saltos  < 2 && esp != ' ' && header[i] != '\n'){
-      width[i-cont] = header[i];
+      auxw[i-cont] = header[i];
+      auxw[i-cont+1] = '\0';
     }else{
       if(saltos > 0 && saltos  < 2 && esp == ' ' && header[i] != esp){
-        height[i-cont] = header[i];
+        auxh[i-cont] = header[i];
+        auxh[i-cont+1] = '\0';
       }
     }
 
@@ -122,8 +136,12 @@ void CutHeader(unsigned char header[],char width[],char height[],int len){
     printf("El archivo ingresado no corresponer a el formato PPM 6\n");
     exit(1);
   }
+  *width = CharToInt(auxw,LenChar(auxw)-1);
+  *height = CharToInt(auxh,LenChar(auxh)-1);
 }
 
+/*Aumenta o adisminuye el zoom de*
+ * la imagen.                    */
 void ImgZoom(float* zoom){
     if(IsKeyPressed(KEY_Z)){
       *zoom += 0.1f;
@@ -135,27 +153,42 @@ void ImgZoom(float* zoom){
     }
 }
 
+/*Calcula la cantidad el zoom inicial tomando*
+* en acuenta las dimensiones de la ventana.  */
+void InitZoom(float *zoom,int width,int height){
+  if(ANCHO < ALTO  && height > ALTO){
+    *zoom = (float)ALTO / height;
+  }else{
+    if(ANCHO > ALTO && width > ANCHO){
+      *zoom = (float)ANCHO/width;
+    }
+  }
+
+}
+
+/*Retorna el tamaño del archivo*/
+uint32_t SizeFile(FILE *f){
+
+  //Voy al final del archivo
+  fseek(f,0L,SEEK_END);
+  //Guardo la longitud del archivp
+  uint32_t size = ftell(f);
+  //Vuelvo al inicio del archivo
+  fseek(f,0L, SEEK_SET);
+  return size;
+}
+
 int main(){
   FILE *f;
   f = fopen("simple.ppm","rb");
-  //Todo: Esto esta mal, pero encontramos el error.
-  unsigned char header[15];
-  fread(header,sizeof(header),1,f);
   if(f == NULL){
     printf("Archivo no encontrado\n");
     exit(1);
   }
-  //int fin = LenHeader(header);
-  Color c;
-  char height[4];
-  char width[4];
-  int len = sizeof(header)/sizeof(header[0]);
-  CutHeader(header,width,height, len);
-  //TOdo: arreglar el problema con la longitud de los numeros
-  int lw = sizeof(width)/sizeof(width[0]) -2;
-  int lh = sizeof(width)/sizeof(width[0]) -2;
-  int w = CharToInt(width, lw);
-  int h = CharToInt(height, lh);
+  uint32_t size = SizeFile(f);
+  unsigned char* buffer = (unsigned char*)malloc(size);
+  fread(buffer,size,1,f);
+  fclose(f);
   Camera2D cam = {
     .zoom = 1.f,
     .offset = {
@@ -168,28 +201,26 @@ int main(){
     },
     .rotation = 0,
   };
-
-  uint32_t size = (w*h)*3;
-  unsigned char* buffer = (unsigned char*)malloc(size);
-  /*if(buffer == NULL){
-    printf("Algo salio mal\n");
-    exit(1);
-  }*/
-  fread(buffer,size,1,f);
-  fclose(f);
- InitWindow(ANCHO,ALTO, "vamos a ver que es lo que pasa");
+  // Guardo la longitud del heder de la imagen.
+  int lh = LenHeader(buffer);
+  int sw = 0;
+  int sh = 0;
+  Color c;
+  CutHeader(buffer,&sw,&sh, lh);
+  InitZoom(&cam.zoom,sw,sh);
+  printf("%d %d\n",size,lh);
+  InitWindow(ANCHO,ALTO, "vamos a ver que es lo que pasa");
   SetTargetFPS(60);
- while (!WindowShouldClose()) {
-   BeginDrawing();
-   ClearBackground(RAYWHITE);
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
     BeginMode2D(cam);
     ImgZoom(&cam.zoom);
-    DrawImg2(buffer,c,w,h,size);
+    DrawImg2(buffer,c,sw,sh,lh,size);
     EndMode2D();
-   EndDrawing();
- }
- CloseWindow();
- 
+    EndDrawing();
+  }
+  CloseWindow();
+
   return 0;
-  
 }
